c6edb53bfbc71e2345cd14289dcc5c653d15b921
Lucas angelo <angelolucas374@gmail.com>
2024-03-12 14:55:37 -0300

    Feat:foi importado na home  housing-location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Localização habitação
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 @Component({
   selector: 'app-home',
   template: `
@@ -8,6 +11,9 @@ import { Component } from '@angular/core';
       <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.scss']
